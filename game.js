b34b010a567438251534b848ff2da43220773d51
LorenzoWynberg <lwcincr93@gmail.com>
2021-07-08 09:49:07 -0600

    Added score

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ loadSprite('blue-steel', 'gqVoI2b.png')
 loadSprite('blue-evil-shroom', 'SvV4ueD.png')
 loadSprite('blue-surprise', 'RMqCc1G.png')
 
-scene("game", () => {
+scene("game", ({score}) => {
 	layers(['bg','obj','ui'], 'obj')
 
 	const map = [
@@ -67,11 +67,11 @@ scene("game", () => {
 	const gameLevel = addLevel(map, levelCfg)
 
 	const scoreLabel = add([
-		text('test'),
+		text(score),
 		pos(30,6),
 		layer('ui'),
 		{
-			value: 'test',
+			value: score,
 		}
 	])
 
@@ -173,4 +173,4 @@ scene('lose', ({score}) =>{
 	add([text(score,32), origin('center'), pos(width()/2, height()/2)])
 })
 
-start("game");
+start("game", {score: 0});
